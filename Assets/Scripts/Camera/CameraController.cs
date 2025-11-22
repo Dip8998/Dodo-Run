7513bf76fd6914d5fabbfa71c8a7176d45b15d39
Dip Patel <pateldeep2332@gmail.com>
2025-11-22 20:50:45 +0530

    Added Skybox for environment

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,33 +5,30 @@ namespace DodoRun.Camera
 {
     public class CameraController : MonoBehaviour
     {
-        [SerializeField] private Transform player;
-        [SerializeField] private Vector3 playerOffset;
-        [SerializeField] private float followSmoothness;
-        [SerializeField] private float roatationSmoothness;
+        [SerializeField] private Vector3 playerOffset = new Vector3(0f, 2.2f, -4.8f);
+        [SerializeField] private float followSmoothness = 0.08f;
+        [SerializeField] private float rotationSmoothness = 8f;
 
         private Transform currentTarget;
         private Vector3 velocity = Vector3.zero;
-
 
         private void Awake()
         {
             GameService.Instance.EventService.OnPlayerSpawned.AddListner(BindToPlayer);
         }
 
-        private void OnDestroy() => GameService.Instance?.EventService?.OnPlayerSpawned?.RemoveListner(BindToPlayer);
+        private void OnDestroy() =>
+            GameService.Instance?.EventService?.OnPlayerSpawned?.RemoveListner(BindToPlayer);
 
         private void LateUpdate()
         {
-            if(currentTarget == null) return;
+            if (currentTarget == null) return;
 
             Vector3 desiredPos = currentTarget.position + playerOffset;
-
             transform.position = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, followSmoothness);
 
-            Quaternion desiredRot = Quaternion.LookRotation(currentTarget.forward);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, roatationSmoothness * Time.deltaTime);
+            Quaternion desiredRot = Quaternion.LookRotation(Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, rotationSmoothness * Time.deltaTime);
         }
 
         private void BindToPlayer(Transform player)
